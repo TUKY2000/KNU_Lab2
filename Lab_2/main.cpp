8e3b7906b9bcf76a01c505b48d8b6f8f02cc512d
IvanMartsilenko <ivanmartsilenko@gmail.com>
2018-10-16 23:29:59 +0300

    Added comments, added plan for main, still working on Jakobi method

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//	to do list																					//
+//	1) need to add variant for random values inputing in matrix 								//
+//	2) need to add realization for diferent methods	(add Enum, add user interface				//
+//	3)exceptions																				//
+//	4) copyright																				//
+//																								//
+//																								//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
 	CMatrix *m = new CMatrix;
@@ -13,10 +25,24 @@ int main()
 	cout << endl; 
 	cout << "Please input number of rows in your matrix:";
 	cin >> iRows;
-	//
 	cout << endl;
 	m->setSize(iRows, iCols);
+	// inputing matrix 
 	cin >> *m;
+	// Overcharged operator >> 
+	/*
+		for (size_t col = 0; col < matr.getCols(); ++col)
+	{
+		std::cout << "Please input collum number" << col << "." << std::endl;
+		for (size_t row = 0; row < matr.getRows(); ++row)
+		{
+			std::cout << "please input value of x[" << row  << "]["<<col << "] " << ":";
+			input >> std::to_string(matr[row][col]);
+			std::cout << std::endl;
+		}
+	}
+	return input;
+	*/
 
 	return 0;
 }
